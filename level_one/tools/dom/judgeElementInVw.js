cc343266df90a6229c12f13f6808eebd35237541
smallsky2516 <smallsky2516@yeah.net>
2022-11-25 22:33:42 +0800

    检测元素在垂直方向上是否在视口内

--- level_one/tools/dom/judgeElementInVw.js
+++ level_one/tools/dom/judgeElementInVw.js
@@ -16,23 +16,6 @@ function judgeElementXDireInVW(element, vwWidth) {
 }
 
 /**
- * 检测元素垂直方向上是否在视口内
- * @param {Element} element 检测的dom元素
- * @param {number} vwHeight 视口的高度
- * @returns 检测的dom元素垂直方向上是否在视口内，true：在，false：不在
- */
-function judgeElementYDireInVW(element, vwHeight) {
-  var clientRect = element.getBoundingClientRect();
-  var top = clientRect.top;
-  var bottom = clientRect.bottom;
-
-  var topIsInVW = top >= 0 && top < vwHeight;
-  var bottomIsVW = bottom > 0 && bottom <= vwHeight;
-
-  return topIsInVW && bottomIsVW;
-}
-
-/**
  * 检测元素是否在视口内
  * @param {Element} element 要检测的dom元素
  * @param {number} vwWidth 视口宽度
