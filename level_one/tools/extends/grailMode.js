edcb86983eaab3608ebb60d315c4f35a770461a2
smallsky2516 <smallsky2516@yeah.net>
2022-11-28 00:36:32 +0800

    继承: 圣杯模式

---
+++ level_one/tools/extends/grailMode.js
@@ -0,0 +1,25 @@
+/**
+ * 圣杯模式，实现继承(兼容性最佳)
+ * @param  {function} Parent 父构造器
+ * @param  {function} Child  子构造器
+ * @return {undefined} 默认返回undef
+ */
+var inherit = (function() {
+    var F = function() {};
+
+    return function(Child, Parent) {
+        F.prototype = Parent.prototype;
+        Child.prototype = new F();
+        Child.prototype.constructor = Child;
+        Child.prototype.uber = Parent.prototype;
+        Child.prototype.super = Parent;
+    };
+}());
+
+
+// var inherit = function(Child, Parent){
+//     Child.prototype = Object.create(Parent.prototype);
+//     Child.prototype.constructor = Child;
+//     Child.prototype.uber = Parent.prototype;
+//     Child.prototype.super = Parent;
+// }；
