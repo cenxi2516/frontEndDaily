d8303851e39b50dc83090e42c07f9c066b2faf3e
smallsky2516 <smallsky2516@yeah.net>
2022-12-10 09:35:28 +0800

    优化深度优先算法

--- level_one/tools/depthFirstSearch/index.js
+++ level_one/tools/depthFirstSearch/index.js
@@ -8,12 +8,14 @@ const depthFirstSearch = (() => {
   return (data, fn) => {
     if (isObject(data)) {
       for (const key in data) {
-        const value = data[key];
-        if (isObject(value)) {
-          depthFirstSearch(value, fn);
-          data[key] = isFunction(fn) ? fn.call(data, key, value) : value;
-        } else {
-          data[key] = isFunction(fn) ? fn.call(data, key, value) : value;
+        if (Object.prototype.hasOwnProperty.call(data, key)) {
+          const value = data[key];
+          if (isObject(value)) {
+            depthFirstSearch(value, fn);
+            data[key] = isFunction(fn) ? fn.call(data, key, value) : value;
+          } else {
+            data[key] = isFunction(fn) ? fn.call(data, key, value) : value;
+          }
         }
       }
 
